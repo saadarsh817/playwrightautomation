e962db058b1395a1f85a1331a68a1587c4486f01
aadarshroadcast <aadarsh.singh@roadcast.net>
2025-05-20 18:47:06 +0530

    fdsaf

---
+++ tests/card_details.spec.js
@@ -0,0 +1,14 @@
+export const card_details = {card_number :'1234567890',
+    month : '05',
+    date : '22',
+    cvv_code : '325',
+    card_name : 'visa',
+    coupon_code : 'atx346',
+    shiping_details_email : 'date@gmail.com',
+    country_name : 'India'
+}
+
+
+export const file_download = {
+    file_name :'Click To Download Order Details in CSV'
+}
